654eebacbcfa996a0900f240139ede34f3ba3eb0
Lov Verma <lovverma001@gmail.com>
2023-08-28 13:28:13 +0530

    [kafka-local]: initial commit

---
+++ src/main/java/com/local/kafka/domain/User.java
@@ -0,0 +1,11 @@
+package com.local.kafka.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+   private String name;
+   private String age;
+
+}
---
+++ src/test/java/com/local/kafka/KafkaApplicationTests.java
@@ -0,0 +1,10 @@
+package com.local.kafka;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+
+
+}
